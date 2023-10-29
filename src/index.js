524d5865128b95d3589ea464a4d33c36508cb52e
Julio Garcia <jgarc5394@gmail.com>
2023-10-29 15:11:40 -0500

    Added API route and changed helpers to utils

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './helpers/dotenv'
+import './utils/dotenv'
 
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 
-import logger from './helpers/logger'
+import logger from './utils/logger'
 import router from './routes'
-import { notFound, errorHandler } from './helpers/errors'
+import { notFound, errorHandler } from './utils/errors'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
@@ -22,6 +22,4 @@ app.use(router)
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () =>
-  logger.info(`Application started at http://localhost:${process.env.PORT}`),
-)
+app.listen(port, () => logger.info(`Server running on port ${port}`))
